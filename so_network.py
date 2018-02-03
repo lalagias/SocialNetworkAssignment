869fcf8334f2c94afedcd7aed1d94738471ac12b
Marios Panagopoulos <mariospanag96@gmail.com>
2018-02-03 17:51:33 +0200

    Add files via upload

--- so_network.py
+++ so_network.py
@@ -62,6 +62,8 @@ if centralities == "0":
         degree_centrality.append(nx.degree_centrality(graphs[i]))
         plt.bar(range(len(degree_centrality[i])), list(degree_centrality[i].values()), align='center')
         plt.xticks(range(len(degree_centrality[i])), list(degree_centrality[i].keys()))
+        plt.title("Graph " + str(i))
+        plt.ylabel('Degree Centrality')
         plt.show()
 
     # 2) In-Degree Centrality
@@ -70,6 +72,8 @@ if centralities == "0":
         in_degree_centrality.append(nx.in_degree_centrality(graphs[i]))
         plt.bar(range(len(in_degree_centrality[i])), list(in_degree_centrality[i].values()), align='center')
         plt.xticks(range(len(in_degree_centrality[i])), list(in_degree_centrality[i].keys()))
+        plt.title("Graph " + str(i))
+        plt.ylabel('In-Degree Centrality')
         plt.show()
 
     # 3) Out-Degree Centrality
@@ -78,6 +82,8 @@ if centralities == "0":
         out_degree_centrality.append(nx.out_degree_centrality(graphs[i]))
         plt.bar(range(len(out_degree_centrality[i])), list(out_degree_centrality[i].values()), align='center')
         plt.xticks(range(len(out_degree_centrality[i])), list(out_degree_centrality[i].keys()))
+        plt.title("Graph " + str(i))
+        plt.ylabel('Out-Degree Centrality')
         plt.show()
 
     # 4) Clossness Centrality
@@ -86,6 +92,8 @@ if centralities == "0":
         closeness_centrality.append(nx.closeness_centrality(graphs[i]))
         plt.bar(range(len(closeness_centrality[i])), list(closeness_centrality[i].values()), align='center')
         plt.xticks(range(len(closeness_centrality[i])), list(closeness_centrality[i].keys()))
+        plt.title("Graph " + str(i))
+        plt.ylabel('Clossness Centrality')
         plt.show()
 
     # 5) Betweenness Centrality
@@ -94,6 +102,8 @@ if centralities == "0":
         betweenness_degree_centrality.append(nx.betweenness_centrality(graphs[i]))
         plt.bar(range(len(betweenness_degree_centrality[i])), list(betweenness_degree_centrality[i].values()), align='center')
         plt.xticks(range(len(betweenness_degree_centrality[i])), list(betweenness_degree_centrality[i].keys()))
+        plt.title("Graph " + str(i))
+        plt.ylabel('Betweenness Centrality')
         plt.show()
 
     # 6) Eigenvector Centrality
@@ -102,16 +112,19 @@ if centralities == "0":
         eigenvector_degree_centrality.append(nx.eigenvector_centrality_numpy(graphs[i]))
         plt.bar(range(len(eigenvector_degree_centrality[i])), list(eigenvector_degree_centrality[i].values()), align='center')
         plt.xticks(range(len(eigenvector_degree_centrality[i])), list(eigenvector_degree_centrality[i].keys()))
+        plt.title("Graph " + str(i))
+        plt.ylabel('Eigenvector Centrality')
         plt.show()
 
     # 7) Katz Centrality
-    katz_degree_centrality = {}
+    katz_degree_centrality = []
     for i in range(0, N):
-        katz_degree_centrality[i] = nx.katz_centrality_numpy(graphs[i])
+        katz_degree_centrality.append(nx.katz_centrality_numpy(graphs[i]))
         plt.bar(range(len(katz_degree_centrality[i])), list(katz_degree_centrality[i].values()), align='center')
         plt.xticks(range(len(katz_degree_centrality[i])), list(katz_degree_centrality[i].keys()))
+        plt.title("Graph " + str(i))
+        plt.ylabel('Katz Centrality')
         plt.show()
-
 
 # 5th Question
 
@@ -119,51 +132,48 @@ if centralities == "0":
 similarities = input("\nType 0 to compute prediction success rates or 1 to skip")
 
 if similarities == "0":
-    # graph vars contains the neighbors
-    # common_nodes contains the intersection of graphs
-    # graph_pairs is a list of dictionaries of the common nodes containing the neighbors of each subgraph
-    # .neighbors returns only succesors nodes not predeccesors
-    # We checked the source nodes, if they got neighbors and are succesors.
-
-    # In general, we found the neighbors for the first subgraph and the neighbors for the second subgraph only for source nodes.
+    # Graph vars contains only the source nodes since neighbors()
+    # returns only succesors nodes and not predeccesors.
+    # graph_pairs is a list of dictionaries of the common nodes where
+    # key is the source node and value is a list of size = N-1 containing
+    # the edges of source node for each interval.
     graph_pairs = []
     for i in range(0, N - 1):
-        graph1 = [x for x in graphs[i].nodes if any(graphs[i].neighbors(x))]
-        graph2 = [x for x in graphs[i+1].nodes if any(graphs[i+1].neighbors(x))]
-        common_nodes = set(graph1).intersection(graph2)
+        graph_1 = [x for x in graphs[i].nodes if any(graphs[i].neighbors(x))]
+        graph_2 = [x for x in graphs[i+1].nodes if any(graphs[i+1].neighbors(x))]
+        common_nodes = set(graph_1).intersection(graph_2)
         graph_pairs.append( dict( (source_id, [list(graphs[i].edges(source_id)), list(graphs[i+1].edges(source_id)) ]) for source_id in common_nodes) )
+    #print(graph_pairs[0])
 
     # Graph creation
-    # sub_graphs is a list that contains the sub graphs
+    # sub_graphs is a list of graphs for each time interval
+    # that contains only the common nodes
     sub_graphs = []
-    # we iterate from 0 to N-1 and we create a Directed Graph every iteration
+    # Create Directed Graph from graph_pairs dictionaries
     for i in range(0, N-1):
         sub_directed_graph = nx.DiGraph()
-        # we iterate all the keys in tha graph_pairs dictionary from the last question
         for j in graph_pairs[i]:
-            # var edges contains the first list of edges(edges that existed in the previous time period)
+            # edges contains the first list (edges of first time interval of pair)
             edges = graph_pairs[i][j][0]
-            # we add the edges in the sub_directed_graph
             sub_directed_graph.add_edges_from(edges)
-        # we append the final sub_directed_graph in the sub_graphs list
         sub_graphs.append(sub_directed_graph)
-    list(sub_graphs[0].nodes)
 
     # 6th Question
 
     # Graph Distance
-    length_shortest_path = []
+    sp_length_list = []
     for i in range(0, N-1):
         shortest_path_iterator = dict(nx.all_pairs_shortest_path_length(sub_graphs[i]))
-        length_shortest_path.append(shortest_path_iterator)
+        sp_length_list.append(shortest_path_iterator)
 
     # Common Neighbors
-    sub_common_neighbors_list = []
+    common_neighbors_list = []
     for i in range(0, N-1):
-        sub_common_neighbors = {u:  {v: len(set(sub_graphs[i].successors(u)).intersection(set(sub_graphs[i].successors(v)))) for v in list(sub_graphs[i].nodes) } for u in list(sub_graphs[i].nodes) }
-        sub_common_neighbors_list.append(sub_common_neighbors)
+        common_neighbors = {u:  {v: len(set(sub_graphs[i].successors(u)).intersection(
+            set(sub_graphs[i].successors(v)))) for v in list(sub_graphs[i].nodes) } for u in list(sub_graphs[i].nodes) }
+        common_neighbors_list.append(common_neighbors)
 
-    # Convert Digraph to undirected
+    # Convert Digraph to undirected to calculate next similarity measures
     sub_graphs_undirected = []
     for i in range(0, N-1):
         sub_graphs_undirected_graph = nx.Graph()
@@ -175,33 +185,33 @@ if similarities == "0":
     jaccard_list = []
     for i in range(0, N-1):
         jaccard_iterator = nx.jaccard_coefficient(sub_graphs_undirected[i])
-        dummylist = []
+        iterator_to_list = []
         for j in jaccard_iterator:
             x, y, z = j
-            dummylist.append((x, y ,z))
-        jaccard_dictionary = { x[0]: { y[1]: y[2] for y in dummylist if y[0] == x[0] } for x in dummylist }
+            iterator_to_list.append((x, y ,z))
+        jaccard_dictionary = { x[0]: { y[1]: y[2] for y in iterator_to_list if y[0] == x[0] } for x in iterator_to_list }
         jaccard_list.append(jaccard_dictionary)
 
     # Adamic/Adar
     adamic_list = []
     for i in range(0, N-1):
         adamic_iterator = nx.adamic_adar_index(sub_graphs_undirected[i])
-        dummylist = []
+        iterator_to_list = []
         for j in adamic_iterator:
             x, y, z = j
-            dummylist.append((x, y ,z))
-        adamic_dictionary = { x[0]: { y[1]: y[2] for y in dummylist if y[0] == x[0] } for x in dummylist }
+            iterator_to_list.append((x, y ,z))
+        adamic_dictionary = { x[0]: { y[1]: y[2] for y in iterator_to_list if y[0] == x[0] } for x in iterator_to_list }
         adamic_list.append(adamic_dictionary)
 
     # Preferential Attachment
     preferential_attachment_list = []
     for i in range(0, N-1):
         preferential_attachment_iterator = nx.preferential_attachment(sub_graphs_undirected[i])
-        dummylist = []
+        iterator_to_list = []
         for j in preferential_attachment_iterator:
             x, y, z = j
-            dummylist.append((x, y ,z))
-        preferential_attachment_dictionary = { x[0]: { y[1]: y[2] for y in dummylist if y[0] == x[0] } for x in dummylist }
+            iterator_to_list.append((x, y ,z))
+        preferential_attachment_dictionary = { x[0]: { y[1]: y[2] for y in iterator_to_list if y[0] == x[0] } for x in iterator_to_list }
         preferential_attachment_list.append(preferential_attachment_dictionary)
 
     # 7th Question
@@ -213,37 +223,44 @@ if similarities == "0":
     pA = float(input("\nType percentage of top for Adamic Adar"))
     pPA = float(input("\nType percentage of top for Preferential Attachment"))
 
-    # List of dictionaries with similarity measures for every subgraph
-    similarity_measures_dicts = { 'pGD': length_shortest_path, 'pCN':
-        sub_common_neighbors_list , 'pJC': jaccard_list, 'pA': adamic_list ,'pPA': preferential_attachment_list }
+    # List of dictionaries with similarity measures for each pair of graphs
+    similarity_measures_dicts = { 'pGD': sp_length_list, 'pCN':
+        common_neighbors_list , 'pJC': jaccard_list, 'pA': adamic_list ,'pPA': preferential_attachment_list }
     # Percentage of top similarity measures
     similarity_measures_top = { 'pGD': pGD, 'pCN': pCN , 'pJC': pJC, 'pA': pA ,'pPA': pPA }
     # Dictionary with final results for each measure
     similarity_measures_final_results = { }
-    # Each iteration add final result of similarity measure combining results
-    # of pairs of graphs.
+    # Dictionary containing number of top edges, number of edges in next interval
+    # and successes for each measure
+    top_edges_successes_dict = { }
+    # Each iteration calculates results for each pair of graphs
     for measure, top in similarity_measures_top.items():
-        top_int = int(sub_graphs[i].size()*top)
         pair_graph_results = []
+        top_edges_successes = []
         # Calculate success rate for each pair of graphs
         for i in range(0, N-1):
+            top_int = int(sub_graphs[i].size()*top)
             max_dict = {}
-            # Find max similarity measure for each key
+            # Find max similarity measure for each key's edges
             for key, value in similarity_measures_dicts[measure][i].items():
                 max_key = max(value.items(), key=operator.itemgetter(1))[0]
                 max_dict[(key,max_key)] = value[max_key]
-            # Get top% edges based on similarity measures
+            # Get final top% edges based on similarity measures
             max_dict = dict(sorted(max_dict.items(), key=operator.itemgetter(1), reverse=True)[:top_int])
+            # Delete edges where similarity measure is 0 or 0.0
+            max_dict = {edge:similarity for edge,similarity in max_dict.items() if similarity!=0.0 and similarity!=0}
             successes = 0
+            # Edges that exist in the second time interval of pair
+            next_interval_edges = [ edges[1][i] for edges in graph_pairs[i].values() for i, val in enumerate(edges[1]) ]
+            # Count successes of top edges (top edges that exist in second time interval)
             for edge in max_dict.keys():
-                if edge in [ edges[1][i] for edges in graph_pairs[i].values() for i, val in enumerate(edges[1]) ]:
+                if edge in next_interval_edges:
                     successes += 1
-            pair_graph_results.append(successes / top_int)
+            pair_graph_results.append(successes / len(next_interval_edges))
+            top_edges_successes.append( (len(max_dict), len(next_interval_edges), successes) )
         similarity_measures_final_results[measure] = pair_graph_results
+        top_edges_successes_dict[measure] = top_edges_successes
+    print("\n")
     print(similarity_measures_final_results)
-
-# NA ALLAKSOUME KANA ONOMA SAN TO DUMMY LIST
-# ANTE KANA SXOLIO
-# FOR PRINT SIMILARITIES SAN SXOLIO
-# WORD ME EKSIGISEIS NA DOSOUME TA RESTA
-# NA TELIOSOUME GIA NA PAME VOLTA
+    print("\n")
+    print(top_edges_successes_dict)
